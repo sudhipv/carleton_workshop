7fc62e25ff8f85daecca2548fa97622b8ab8fdc1
user01 user01 <user01@login1.int.edia-crltn.c3.ca>
2025-03-21 16:25:25 +0000

    tested from original system

--- codes/run_code.sh
+++ codes/run_code.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=def-sponsor00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=16
 #SBATCH --time=00:10:00
 ##SBATCH --mem-per-cpu=3700M
-#SBATCH --job-name=mpi_parallel_factorize
+#SBATCH --job-name=parallel_48_factorize
 #SBATCH --output=%x-%j.out
 #SBATCH --mail-user=sudhipv@cmail.carleton.ca
 #SBATCH --mail-type=ALL
@@ -16,9 +16,10 @@ module load scipy-stack
 
 ### For running the serial code
 # python serial.py
-# python serial_factorize.py
+#python serial_factorize.py
 
 ### For running the parallel MPI code
 # mpiexec -n 8 python parallel.py
-mpiexec -n 2 python parallel_factorize.py
+#mpiexec -n 8 python mpi_helloworld.py
+mpiexec -n 40 python parallel_factorize.py
 
--- codes/run_code_GPU.sh
+++ codes/run_code_GPU.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --account=def-sponsor00
-#SBATCH --gres=gpu:v100:1
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=3
 #SBATCH --time=00:10:00
 ##SBATCH --mem-per-cpu=3700M
-#SBATCH --job-name=GPU_run
+#SBATCH --job-name=GPU_cnn
 #SBATCH --output=%x-%j.out
 #SBATCH --mail-user=sudhipv@cmail.carleton.ca
 #SBATCH --mail-type=ALL
 #SBATCH --mail-type=FAIL
 
 
-
-nvidia-smi
+python CNN.py
+#nvidia-smi
